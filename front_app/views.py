1ec4c46b5e74ed261f3a5bac19594749f5bef794
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-05-11 11:11:57 +0300

    Add product-detail page

--- front_app/views.py
+++ front_app/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,"front_app/index.html", {})
+
+def product_detail(request):
+    return render(request, "front_app/product-detail.html", {})
