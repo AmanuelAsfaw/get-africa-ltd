cfcc9277ec957e5fd339a3964d7ec16e29aa07a6
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-05-11 15:05:47 +0300

    Register Models in Admin

--- front_app/admin.py
+++ front_app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from front_app.models import Catagory, Message, Product, ProductImage, Promotion, Response, Service, Sponser, Testimonial
+
 # Register your models here.
+
+admin.site.register(Catagory)
+admin.site.register(Message)
+admin.site.register(Response)
+admin.site.register(Promotion)
+admin.site.register(Sponser)
+admin.site.register(Product)
+admin.site.register(ProductImage)
+admin.site.register(Service)
+admin.site.register(Testimonial)
